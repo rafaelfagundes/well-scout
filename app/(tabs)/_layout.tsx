46a86123a073ec04f58dcc5a3747685f3261ce0a
Rafael Fagundes <rafaelcflima@gmail.com>
2025-02-14 21:09:07 -0500

    refactor: Improve tab bar styling and remove TabBarBackground component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Platform, View } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
-import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { Swap, Cube, Barcode, Gauge, MagnifyingGlass } from 'phosphor-react-native';
@@ -15,59 +14,58 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <View
-      style={{ backgroundColor: "red", height: "100%", width: "100%" }}
-    >
-      <Tabs
-        screenOptions={{
-          tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-          headerShown: false,
-          tabBarButton: HapticTab,
-          tabBarBackground: TabBarBackground,
-          tabBarStyle: Platform.select({
-            ios: {
-              // Use a transparent background on iOS to show the blur effect
-              position: 'absolute',
-            },
-            default: {},
-          }),
-        }}>
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: 'Products',
-            tabBarIcon: ({ color }) => <Cube color={color} size={ICON_SIZE} />
-          }}
-        />
-        <Tabs.Screen
-          name="swap"
-          options={{
-            title: 'Swap',
-            tabBarIcon: ({ color }) => <Swap color={color} size={ICON_SIZE} />
-          }}
-        />
-        <Tabs.Screen
-          name="scan"
-          options={{
-            title: 'Scan',
-            tabBarIcon: ({ color }) => <Barcode color={color} size={ICON_SIZE} />
-          }}
-        />
-        <Tabs.Screen
-          name="overview"
-          options={{
-            title: 'Overview',
-            tabBarIcon: ({ color }) => <Gauge color={color} size={ICON_SIZE} />
-          }}
-        />
-        <Tabs.Screen
-          name="search"
-          options={{
-            title: 'Search',
-            tabBarIcon: ({ color }) => <MagnifyingGlass size={ICON_SIZE} color={color} />
-          }}
-        />
-      </Tabs>
-    </View>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: undefined,
+        tabBarLabelStyle: {
+          marginTop: 5,
+          fontFamily: 'SansationBold',
+          fontSize: 14
+        },
+        tabBarStyle: {
+          backgroundColor: 'transparent',
+          position: 'absolute',
+          margin: 10
+        },
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Products',
+          tabBarIcon: ({ color }) => <Cube color={color} size={ICON_SIZE} />
+        }}
+      />
+      <Tabs.Screen
+        name="swap"
+        options={{
+          title: 'Swap',
+          tabBarIcon: ({ color }) => <Swap color={color} size={ICON_SIZE} />
+        }}
+      />
+      <Tabs.Screen
+        name="scan"
+        options={{
+          title: 'Scan',
+          tabBarIcon: ({ color }) => <Barcode color={color} size={ICON_SIZE} />
+        }}
+      />
+      <Tabs.Screen
+        name="overview"
+        options={{
+          title: 'Overview',
+          tabBarIcon: ({ color }) => <Gauge color={color} size={ICON_SIZE} />
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: 'Search',
+          tabBarIcon: ({ color }) => <MagnifyingGlass size={ICON_SIZE} color={color} />
+        }}
+      />
+    </Tabs>
   );
 }
