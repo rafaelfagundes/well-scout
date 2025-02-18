4bfcabccb6f8150290209e4b67671d109a9138e0
Rafael Fagundes <rafaelcflima@gmail.com>
2025-02-18 11:14:00 -0500

    feat: display product details and Nutri-Score

--- features/products/ProductItem.tsx
+++ features/products/ProductItem.tsx
@@ -1,6 +1,8 @@
 import { View, Text, Image, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { ArrowUp, Clock } from 'phosphor-react-native';
+import { Fonts } from '@/constants/Fonts';
+import NutriAndEcoScore from './NutriAndEcoScore';
 
 export default function ProductRating() {
   return (
@@ -17,29 +19,20 @@ export default function ProductRating() {
                 resizeMode="cover"
               />
             </View>
-            <View style={styles.textContainer}>
-              <Text style={styles.productName}>Ketchup</Text>
-              <Text style={styles.brandName}>Heinz</Text>
+            <View style={styles.infoContainer}>
+              <View>
+                <Text style={styles.productName}>Ketchup</Text>
+                <Text style={styles.brandName}>Heinz</Text>
+              </View>
+              <NutriAndEcoScore nutriScore='b' ecoScore='c'></NutriAndEcoScore>
             </View>
           </View>
           <View style={styles.timeContainer}>
-            <Clock size={16} color="#6b7280" />
+            <Clock size={12} color="#6b7280" />
             <Text style={styles.timeText}>4 hours ago</Text>
           </View>
         </View>
 
-        <View style={styles.gradientContainer}>
-          <LinearGradient
-            colors={['#991b1b', '#ef4444', '#facc15', '#4ade80', '#166534']}
-            style={styles.gradient}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 0 }}
-          />
-          <View style={styles.ratingLabel}>
-            <ArrowUp size={16} color="#6b7280" />
-            <Text style={styles.ratingText}>Bad</Text>
-          </View>
-        </View>
       </View>
     </View>
   );
@@ -47,78 +40,62 @@ export default function ProductRating() {
 
 const styles = StyleSheet.create({
   card: {
-    maxWidth: 672,
-    margin: 16,
-    borderRadius: 8,
+    borderRadius: 20,
     backgroundColor: 'white',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 2,
+    // shadowColor: '#000',
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowOpacity: 0.1,
+    // shadowRadius: 4,
+    // elevation: 2,
   },
   cardContent: {
-    padding: 24,
+    padding: 10,
   },
   topContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'flex-start',
-    gap: 24,
   },
   productInfo: {
     flexDirection: 'row',
-    gap: 24,
     flex: 1,
   },
   imageContainer: {
     width: 70,
     height: 70,
-    borderRadius: 8,
+    borderRadius: 16,
     overflow: 'hidden',
     backgroundColor: '#8B2323',
+    marginRight: 10
   },
   image: {
     width: '100%',
     height: '100%',
   },
-  textContainer: {
-    gap: 4,
+  infoContainer: {
+    justifyContent: 'space-between'
   },
   productName: {
-    fontSize: 24,
-    fontWeight: '600',
-    lineHeight: 32,
+    fontFamily: Fonts.sansSerif,
+    fontSize: 15,
+    fontWeight: '700',
   },
   brandName: {
-    fontSize: 20,
+    fontFamily: Fonts.sansSerif,
+    fontSize: 12,
     color: '#6b7280',
+    marginTop: 4
   },
   timeContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 8,
+    gap: 4,
+    marginRight: 10,
+    marginTop: 3
   },
   timeText: {
-    fontSize: 14,
-    color: '#6b7280',
-  },
-  gradientContainer: {
-    marginTop: 24,
-    gap: 8,
-  },
-  gradient: {
-    height: 8,
-    width: '100%',
-    borderRadius: 9999,
-  },
-  ratingLabel: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 4,
-  },
-  ratingText: {
-    fontSize: 14,
+    fontFamily: Fonts.sansSerif,
+    fontSize: 11,
     color: '#6b7280',
   },
   chevron: {
